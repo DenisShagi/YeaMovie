a682a7862059e29c5909ba5dbcd69aa7ba2a691e
Денис Шагимарданов <denisshagimardanov@Deniss-MacBook-Pro.local>
2025-04-19 18:20:54 +0500

    refactor: refactor code

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,18 +4,20 @@ import Spinner from './Spinner'
 const MovieList = ({ movies, errorMessage, loading }) => {
 	return (
 		<section className='all-movies'>
-			<h2 className='text-center mt-[40px]'>All Movies</h2>
 			{loading.search ? (
 				<Spinner />
 			) : errorMessage ? (
 				<p className='text-red-500'>{errorMessage}</p>
-			) : (
-				<ul>
-					{movies.map(movie => (
-						<MovieCard key={movie.id} movie={movie} />
-					))}
-				</ul>
-			)}
+			) : movies.length > 0 ? (
+				<>
+					<h2 className='text-center mt-[40px]'>All Movies</h2>
+					<ul>
+						{movies.map(movie => (
+							<MovieCard key={movie.id} movie={movie} />
+						))}
+					</ul>
+				</>
+			) : null}
 		</section>
 	)
 }
